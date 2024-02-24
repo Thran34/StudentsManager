e74358a0f8b9fa43dfef023d631655128cd06554
andrzejjasinski <thran34@interia.pl>
2024-02-24 13:31:37 +0100

    init

---
+++ StudentsManager/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StudentsManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
